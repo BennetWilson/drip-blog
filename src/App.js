2ee51fbed7ffccaa8603e04de1c0ebcb18290725
Tgarrey37 <tpopp0303@gmail.com>
2022-02-23 22:14:55 -0700

    Made login and register routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import TopBar from './components/topbar/Topbar';
 import './App.css';
-import Single from './pages/single/Single'
+import Single from './pages/single/Single';
 import Write from './pages/write/Write';
 import Settings from './pages/settings/Settings';
-
+import Login from './pages/login/Login';
 import Home from './pages/home/Home';
-
+import Register from './pages/register/Register'
 function App() {
   return (
     <>
     <TopBar />
+    <Login/>
     <Settings />
     </>
    
