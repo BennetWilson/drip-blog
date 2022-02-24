16593722e4f45819ce6cecb7f9be49eac9421eec
Jake Luedke <rajake18@gmail.com>
2022-02-24 13:48:58 -0700

    added icon to topbar

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './header.css'
-import DripCover from '../../assets/Drip-Cover.jpeg'
+import DripCover from '../../Assets/Drip-Cover.jpeg'
 
 
 export default function Header () {
     return (
         <div className='header'>
         <div className="headerTitles">
-        <span classname='headerTitleSm'>We're Passionate About Coffee</span>
-        <span className='headerTitleLg'>DRIP</span>
+        <span className='headerTitleSm'>We're Passionate About Coffee</span>
+        <span className='headerTitleLg'>DRIP <i class="fa-solid fa-droplet"></i></span>
+        
         </div> 
         <img className='headerImg' src={DripCover} alt="" />          
         </div>
