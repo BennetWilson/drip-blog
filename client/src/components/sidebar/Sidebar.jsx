cd41a2f8e07e53ca4f57c15e895b335d475f5504
Tgarrey37 <tpopp0303@gmail.com>
2022-02-24 12:35:49 -0700

    fixed social icons

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -28,10 +28,8 @@ export default function Sidebar() {
       <div className="sideBarItem">
         <span className="sideBarTitle">FOLLOW US</span>
         <div className="sideBarSocial">
-          <i className="sideBarIcon fa-brands fa-facebook-square"></i>
-          <i className="sideBarIcon fa-brands fa-twitter-square"></i>
-          <i className="sideBarIcon fa-brands fa-pinterest-square"></i>
-          <i className="sideBarIcon fa-brands fa-instagram-square"></i>
+        <a target='_blank'href="https://www.linkedin.com/in/taylor-garrey-4934b71a8/">  <i className="topIcon fa-brands fa-linkedin"></i> </a>
+       <a target="_blank" href="https://github.com/BennetWilson/drip-blog"> <i className="topIcon fa-brands fa-github-square"></i> </a>
         </div>
       </div>
     </div>
--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -7,10 +7,8 @@ export default function TopBar() {
   return (
     <div className="top">
       <div className="topLeft">
-        <i className="topIcon fa-brands fa-facebook-square"></i>
-        <i className="topIcon fa-brands fa-twitter-square"></i>
-        <i className="topIcon fa-brands fa-pinterest-square"></i>
-        <i className="topIcon fa-brands fa-instagram-square"></i>
+      <a target='_blank'href="https://www.linkedin.com/in/taylor-garrey-4934b71a8/">  <i className="topIcon fa-brands fa-linkedin"></i> </a>
+       <a target="_blank" href="https://github.com/BennetWilson/drip-blog"> <i className="topIcon fa-brands fa-github-square"></i> </a>
       </div>
       <div className="topCenter">
         <ul className="topList">
