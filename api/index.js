b5a1689a51f12dfecbb3831eba04d4da7d3588db
Tgarrey37 <tpopp0303@gmail.com>
2022-02-24 18:09:41 -0700

    Updated posts routes and categories, started working on fron end axios fetching

--- api/index.js
+++ api/index.js
@@ -3,7 +3,11 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const authRoute = require("./routes/auth");
-const userRoute = require("./routes/users");
+const userRoute = require("./routes/users")
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer =  require("multer");
+
 
 dotenv.config();
 app.use(express.json());
@@ -12,10 +16,25 @@ app.use(express.json());
 mongoose.connect("mongodb://localhost/blog", {
     useNewUrlParser:true,
     useUnifiedTopology: true,
-}).then(console.log("Connected to MongoDB")).catch(err=> console.log(err))
+}).then(console.log("Connected to MongoDB")).catch(err=> console.log(err));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) =>{
+        cb(null,"images")
+    }, filename:(req,file,cb) =>{
+        cb(null,req.body.name)
+    },
+});
+
+const upload = multer ({storage:storage});
+app.post("/api/upload", upload.single("file"), (req,res) => {
+    res.status(200).json("File has been uploaded!")
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
 
 app.listen('3000', () => {
     console.log('Connected at 3000')
