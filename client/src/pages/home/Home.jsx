21f17540094304a32707b8841db5cb2eca3d108b
Tgarrey37 <tpopp0303@gmail.com>
2022-02-24 18:02:30 -0700

    Created more routes and working on client axios fetching now

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,8 +4,19 @@ import './Home.css'
 import Header from '../../components/header/Header'
 import Posts from '../../components/posts/Posts'
 import SideBar from '../../components/sidebar/Sidebar'
+import axios from "axios"
+import {useState, useEffect} from "react"
+
 
 export default function Home () {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() =>{
+        const fetchPosts = async () =>{
+            axios.get("")
+        }
+},[])
+
     return (
         <>
         <Header />
